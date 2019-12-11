b039b9f9c9683b49b08812fca9e7070cb3607cb2
comegitit <aggarwal.roy@gmail.com>
2019-12-11 13:05:58 -0700

    commented app.js

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
+// Define variables only once the page is fully loaded
 window.addEventListener('load', () => {
 
   let lat = 'instantiation'
   let long = 'instantiation'
+
+  // Return the first element within the document that matches the specified selector or group of selectors
   const tempInDegrees = document.querySelector('.temp-in-degrees')
   const ConditionsDescription = document.querySelector('.conditions-description')
   const locationTimezone = document.querySelector('.location-timezone')
 
-  //use built-in javascript functionality to fetch lat & long
+  // Use built-in JavaScript functionality to fetch latitiude & longitude
   navigator.geolocation.getCurrentPosition(position => {
     lat = position.coords.latitude
     long = position.coords.longitude
 
-    //proxy workaround for browser CORS policy restrictions
+    // Proxy workaround for browser CORS policy restrictions
     const proxy = "https://cors-anywhere.herokuapp.com/"
-    //using 'Dark Sky' API with query string for metric units (celsius)
+    //using 'Dark Sky' API with query string for metric units (si = celsius etc.)
     const api = `${proxy}https://api.darksky.net/forecast/22dc744c9e56c0455e206506c8a258f4/${lat},${long}/?units=si`
 
     //fetch API data & convert to JSON
@@ -25,7 +28,8 @@ window.addEventListener('load', () => {
       .then(data => {
         //console.log(data). Leave here for future testing purposes
 
-        const {temperature, summary,icon
+        //Destructuring assignment syntax: An expression that makes it possible to unpack values from arrays (or properties from objects) into distinct variables
+        const {temperature, summary, icon
         } = data.currently
 
         tempInDegrees.textContent = Math.round(temperature)
@@ -37,6 +41,7 @@ window.addEventListener('load', () => {
       })
   })
 
+  //Set the Skycon icons
   function setIcons(icon, iconId) {
     const skycons = new Skycons({
       color: 'white'
@@ -44,6 +49,7 @@ window.addEventListener('load', () => {
 
     //replace dashes with underscores, convert to upper case
     const currentIcon = icon.replace(/-/g, "_").toUpperCase()
+
     //animate 
     skycons.play()
 
